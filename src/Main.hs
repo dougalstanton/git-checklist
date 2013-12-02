18089d849eadc6b4af17726065206a01874d5d50
Dougal Stanton <dougal@dougalstanton.net>
2013-12-02 21:28:28 +0000

    Split actions into observing and destructive
    The old Action type becomes either Act or Observe.  Observe produces a
    String which is written to screen so guarantees nothing bad can happen
    to checklist. The checklist is only altered by the Act type.
    
    Cleaned up a whole bunch of imports and added type signatures to all top
    level definitions. Tried compiling with -Wall but I can't be bothered
    adding a type declaration to [1..] just to make the compiler briefly
    happy. It can wallow in its own misery.

--- src/Main.hs
+++ src/Main.hs
@@ -1,28 +1,27 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import Control.Applicative
-import Control.Exception
+import Control.Exception (IOException, catch)
 import Control.Monad (when)
 
-import Data.Monoid
+import Data.Monoid (mconcat)
 
 import Options.Applicative
 
-import System.Exit
-import System.Environment
+import System.Exit (exitFailure)
 import System.FilePath ((</>))
-import System.Directory
+import System.Directory (createDirectoryIfMissing, doesFileExist, removeFile)
 import System.Process (readProcess)
 
 -- Git stuff. Find the branch we're on and the .git directory.
 
 git :: String -> IO [String]
 git args = lines <$> readProcess "git" (words args) []
-            `catch` \(e :: IOException) -> do
+            `catch` \(_ :: IOException) -> do
                              putStrLn "Something went wrong, maybe you're not in a Git repository?"
                              exitFailure
 
+getBranch, getGitDir :: IO String
 getBranch = head <$> git "symbolic-ref --short HEAD"
 getGitDir = head <$> git "rev-parse --show-cdup" >>= \move -> return $ move ++ ".git"
 
@@ -32,8 +31,8 @@ data ToDo = ToDo { description :: String
                  , complete    :: Bool
                  } deriving (Eq, Read, Show)
 
-data Checklist = Checklist { branch :: String
-                           , todos  :: [ToDo]
+data Checklist = Checklist { branchRef :: String
+                           , todos     :: [ToDo]
                            } deriving (Eq,Show)
 
 -- Loading and saving data (by cheating).
@@ -45,10 +44,10 @@ putChecklist checklist = do
         output  = todos checklist
     createDirectoryIfMissing False listdir
     if null output
-        then removeFile (listdir </> branch checklist) `catch`
-                (\(e :: IOError) -> putStrLn "Error cleaning up checklist file in .git/checklist/")
-        else writeFile (listdir </> branch checklist) (show output)
-                `catch` \(e :: IOException) -> putStrLn "Error writing checklist file."
+        then removeFile (listdir </> branchRef checklist) `catch`
+                (\(_ :: IOError) -> putStrLn "Error cleaning up checklist file in .git/checklist/")
+        else writeFile (listdir </> branchRef checklist) (show output)
+                `catch` \(_ :: IOException) -> putStrLn "Error writing checklist file."
 
 getChecklist :: String -> IO Checklist
 getChecklist branch = do
@@ -61,14 +60,12 @@ getChecklist branch = do
 
 -- Operations: Adding, removing, marking done and not done.
 
-run :: Action -> Checklist -> Checklist
-run (Add desc) (Checklist b ts) = Checklist b (ToDo desc False:ts)
-run (Remove n) (Checklist b ts) = let del = map snd . filter (\i -> n /= fst i)
-                                  in Checklist b (del `withNumbered` ts)
-run (Done   n) (Checklist b ts) = Checklist b (mark True n `withNumbered` ts)
-run (Undo   n) (Checklist b ts) = Checklist b (mark False n `withNumbered` ts)
-run Show       c                = c
-run Stats      c                = c
+change :: Act -> Checklist -> Checklist
+change (Add desc) (Checklist b ts) = Checklist b (ToDo desc False:ts)
+change (Remove n) (Checklist b ts) = let del = map snd . filter (\i -> n /= fst i)
+                                     in Checklist b (del `withNumbered` ts)
+change (Done   n) (Checklist b ts) = Checklist b (mark True n `withNumbered` ts)
+change (Undo   n) (Checklist b ts) = Checklist b (mark False n `withNumbered` ts)
 
 withNumbered :: ([(Int,a)] -> [a]) -> [a] -> [a]
 f `withNumbered` as = f $ zip [1..] as
@@ -80,6 +77,16 @@ mark v n its = map f its
 
 -- Pretty printing! Not very pretty for >9 items.
 
+view :: Observe -> Checklist -> String
+view List  checklist = unlines $ prettyChecklist checklist
+view Stats (Checklist br []) = br ++ ": No tasks defined yet"
+view Stats (Checklist br ts) = br ++ ": " ++ tasksToDo ++ totalTasks
+    where tasksToDo  = case length $ filter (not . complete) ts of
+                            0 -> "Nothing to do! ("
+                            1 -> "1 task left! ("
+                            n -> show n ++ " tasks to do ("
+          totalTasks = show (length ts) ++ " in total)"
+
 prettyTodo :: ToDo -> String
 prettyTodo t = xmark ++ description t
     where xmark = if complete t then "[x] " else "[ ] "
@@ -88,43 +95,37 @@ prettyChecklist :: Checklist -> [String]
 prettyChecklist = zipWith f [1..] . map prettyTodo . todos
     where f i desc = show i ++ ": " ++ desc
 
-printChecklist :: Checklist -> IO ()
-printChecklist = putStr . unlines . prettyChecklist
+-- Overall control actions
 
-printStats :: Checklist -> IO ()
-printStats (Checklist br ts)= putStrLn $ br ++ ": " ++ pending ++ " task to do (" ++ total ++ " total)"
-    where pending = show $ length $ filter (not . complete) ts
-          total    = show $ length ts
-
---
-
-modifyChecklist act branch = do
-    checklist <- getChecklist branch
-    let newlist = run act checklist
-    when (newlist /= checklist) (putChecklist newlist)
-    if act == Stats
-        then printStats newlist
-        else printChecklist newlist
+-- Sometimes the checklist is Left alone and
+-- sometimes we Right on it...
+withBranch :: Either Observe Act -> String -> IO ()
+withBranch (Left act)  branch = getChecklist branch >>= putStr . view act
+withBranch (Right act) branch = do
+    oldlist <- getChecklist branch
+    let newlist = change act oldlist
+    when (newlist /= oldlist)
+         (putChecklist newlist)
+    putStr (view List newlist)
 
 usingArgs :: Option -> IO ()
-usingArgs opts = do
-    branch <- maybe getBranch return $ branchName $ commonOpt opts
-    modifyChecklist (actionOpt opts) branch
+usingArgs (Option (Common Nothing)       act) = getBranch >>= withBranch act
+usingArgs (Option (Common (Just branch)) act) = withBranch act branch
 
 -- Define command line flags and options
 
 data Option = Option { commonOpt :: Common
-                     , actionOpt :: Action
+                     , actionOpt :: Either Observe Act
                      } deriving Show
 
 data Common = Common { branchName :: (Maybe String) } deriving Show
 
-data Action = Show | Stats | Add String | Remove Int | Done Int | Undo Int
-                deriving (Eq, Show)
+data Observe = List | Stats deriving Show
+data Act = Add String | Remove Int | Done Int | Undo Int deriving Show
 
 cli :: Parser Option
 cli = subparser $ mconcat
-            [ command "show" (info (Option <$> common <*> pure Show)
+            [ command "show" (info (Option <$> common <*> showlist)
                                    (progDesc "Show current TODOs"))
             , command "add" (info (Option <$> common <*> add)
                                   (progDesc "Add a TODO"))
@@ -134,13 +135,16 @@ cli = subparser $ mconcat
                                    (progDesc "Item needs redone!"))
             , command "remove" (info (Option <$> common <*> remove)
                                      (progDesc "Remove a TODO (can't be undone)"))
-            , command "stats" (info (Option <$> common <*> pure Stats)
+            , command "stats" (info (Option <$> common <*> showstat)
                                     (progDesc "Summary statistics of checklist"))
             ]
-    where add    = Add . unwords <$> arguments str (metavar "DESCRIPTION")
-          remove = Remove <$> argument auto (metavar "N")
-          done   = Done <$> argument auto (metavar "N")
-          undo   = Undo <$> argument auto (metavar "N")
+    where add    = Right . Add . unwords <$> arguments str (metavar "DESCRIPTION")
+          remove = Right . Remove <$> argument auto (metavar "N")
+          done   = Right . Done <$> argument auto (metavar "N")
+          undo   = Right . Undo <$> argument auto (metavar "N")
+
+          showlist = pure $ Left List
+          showstat = pure $ Left Stats
 
           common = Common <$> (optional $ strOption (long "branch" <> short 'b'
                                                      <> metavar "BRANCH"))
@@ -149,6 +153,7 @@ argParser :: ParserInfo Option
 argParser = info (helper <*> (blank <|> cli))
                     (progDesc "Per-branch TODO list for Git repositories")
     where blank :: Parser Option -- user enters no arguments
-          blank = nullOption (value (Option (Common Nothing) Show) <> internal)
+          blank = nullOption (value (Option (Common Nothing) (Left List)) <> internal)
 
+main :: IO ()
 main = execParser argParser >>= usingArgs
